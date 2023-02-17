61f7a1e060d19542a45f52dfe21bf9c462987f80
Quincy Wilcox <qgwilcox@gmail.com>
2023-02-16 19:15:34 -0700

    Fixed where I was creating and parsing the status when posting a status

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
@@ -37,67 +37,9 @@ public class StatusService {
         executor.execute(getFeedTask);
     }
 
-    public List<String> parseURLs(String post) {
-        List<String> containedUrls = new ArrayList<>();
-        for (String word : post.split("\\s")) {
-            if (word.startsWith("http://") || word.startsWith("https://")) {
-
-                int index = findUrlEndIndex(word);
-
-                word = word.substring(0, index);
-
-                containedUrls.add(word);
-            }
-        }
-
-        return containedUrls;
-    }
-
-    public List<String> parseMentions(String post) {
-        List<String> containedMentions = new ArrayList<>();
-
-        for (String word : post.split("\\s")) {
-            if (word.startsWith("@")) {
-                word = word.replaceAll("[^a-zA-Z0-9]", "");
-                word = "@".concat(word);
-
-                containedMentions.add(word);
-            }
-        }
-
-        return containedMentions;
-    }
-
-    public int findUrlEndIndex(String word) {
-        if (word.contains(".com")) {
-            int index = word.indexOf(".com");
-            index += 4;
-            return index;
-        } else if (word.contains(".org")) {
-            int index = word.indexOf(".org");
-            index += 4;
-            return index;
-        } else if (word.contains(".edu")) {
-            int index = word.indexOf(".edu");
-            index += 4;
-            return index;
-        } else if (word.contains(".net")) {
-            int index = word.indexOf(".net");
-            index += 4;
-            return index;
-        } else if (word.contains(".mil")) {
-            int index = word.indexOf(".mil");
-            index += 4;
-            return index;
-        } else {
-            return word.length();
-        }
-    }
-
-    public void postStatus(String post, MainPresenter.PostStatusObserver observer) {
-        Status newStatus = new Status(post, Cache.getInstance().getCurrUser(), System.currentTimeMillis(), parseURLs(post), parseMentions(post));
+    public void postStatus(Status status, MainPresenter.PostStatusObserver observer) {
         PostStatusTask statusTask = new PostStatusTask(Cache.getInstance().getCurrUserAuthToken(),
-                newStatus, new PostStatusHandler(observer));
+                status, new PostStatusHandler(observer));
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.execute(statusTask);
     }
--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/MainPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/MainPresenter.java
@@ -2,40 +2,17 @@ package edu.byu.cs.tweeter.client.presenter;
 
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.FollowService;
 import edu.byu.cs.tweeter.client.model.service.StatusService;
 import edu.byu.cs.tweeter.client.model.service.UserService;
+import edu.byu.cs.tweeter.model.domain.Status;
 import edu.byu.cs.tweeter.model.domain.User;
 
 public class MainPresenter {
-
-    public void unfollow() {
-        followService.unfollow(selectedUser, new FollowObserver());
-
-    }
-
-    public void follow() {
-        followService.follow(selectedUser, new FollowObserver());
-
-    }
-
-    public void logout() {
-        userService.logout(new LogOutObserver());
-    }
-
-    public void clearCache() {
-        Cache.getInstance().clearCache();
-    }
-
-    public void tryStatusPost(String post, String tag) {
-        try {
-            statusService.postStatus(post, new PostStatusObserver());
-        } catch (Exception ex) {
-            Log.e(tag, ex.getMessage(), ex);
-            view.displayMessage("Failed to post the status because of exception: " + ex.getMessage());
-        }
-    }
 
     public interface View {
 
@@ -92,6 +69,91 @@ public class MainPresenter {
         } else {
             view.setFollowButtonVisibility(true);
             followService.isFollower(selectedUser, new IsFollowerObserver());
+        }
+    }
+
+    public void unfollow() {
+        followService.unfollow(selectedUser, new FollowObserver());
+
+    }
+
+    public void follow() {
+        followService.follow(selectedUser, new FollowObserver());
+
+    }
+
+    public void logout() {
+        userService.logout(new LogOutObserver());
+    }
+
+    public void clearCache() {
+        Cache.getInstance().clearCache();
+    }
+
+    public void tryStatusPost(String post, String tag) {
+        try {
+            Status newStatus = new Status(post, Cache.getInstance().getCurrUser(), System.currentTimeMillis(), parseURLs(post), parseMentions(post));
+            statusService.postStatus(newStatus, new PostStatusObserver());
+        } catch (Exception ex) {
+            Log.e(tag, ex.getMessage(), ex);
+            view.displayMessage("Failed to post the status because of exception: " + ex.getMessage());
+        }
+    }
+
+    public List<String> parseURLs(String post) {
+        List<String> containedUrls = new ArrayList<>();
+        for (String word : post.split("\\s")) {
+            if (word.startsWith("http://") || word.startsWith("https://")) {
+
+                int index = findUrlEndIndex(word);
+
+                word = word.substring(0, index);
+
+                containedUrls.add(word);
+            }
+        }
+
+        return containedUrls;
+    }
+
+    public List<String> parseMentions(String post) {
+        List<String> containedMentions = new ArrayList<>();
+
+        for (String word : post.split("\\s")) {
+            if (word.startsWith("@")) {
+                word = word.replaceAll("[^a-zA-Z0-9]", "");
+                word = "@".concat(word);
+
+                containedMentions.add(word);
+            }
+        }
+
+        return containedMentions;
+    }
+
+    public int findUrlEndIndex(String word) {
+        if (word.contains(".com")) {
+            int index = word.indexOf(".com");
+            index += 4;
+            return index;
+        } else if (word.contains(".org")) {
+            int index = word.indexOf(".org");
+            index += 4;
+            return index;
+        } else if (word.contains(".edu")) {
+            int index = word.indexOf(".edu");
+            index += 4;
+            return index;
+        } else if (word.contains(".net")) {
+            int index = word.indexOf(".net");
+            index += 4;
+            return index;
+        } else if (word.contains(".mil")) {
+            int index = word.indexOf(".mil");
+            index += 4;
+            return index;
+        } else {
+            return word.length();
         }
     }
 

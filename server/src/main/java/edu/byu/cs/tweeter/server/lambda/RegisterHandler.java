bfe2736627d8462cb44d1abeeb5fd882b1f507e1
Quincy Wilcox <qgwilcox@gmail.com>
2023-03-19 11:30:36 -0600

    Get code set up

---
+++ server/src/main/java/edu/byu/cs/tweeter/server/lambda/RegisterHandler.java
@@ -0,0 +1,14 @@
+//package edu.byu.cs.tweeter.server.lambda;
+//
+//import com.amazonaws.services.lambda.runtime.Context;
+//import com.amazonaws.services.lambda.runtime.RequestHandler;
+//
+//import edu.byu.cs.tweeter.model.net.request.LoginRequest;
+//import edu.byu.cs.tweeter.model.net.response.LoginResponse;
+//
+//public class RegisterHandler implements RequestHandler<RegisterRequest, RegisterResponse>  {
+//    @Override
+//    public RegisterResponse handleRequest(RegisterRequest input, Context context) {
+//        return null;
+//    }
+//}

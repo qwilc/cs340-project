86776ec24cb32acc582ffe99604333f1ebbc4ada
qwilc <qgwilcox@gmail.com>
2023-03-19 18:01:07 -0600

    Fixed places that were still using LoginResponse and LoginRequest instead of AuthenticationResponse/Request

--- server/src/main/java/edu/byu/cs/tweeter/server/service/UserService.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/service/UserService.java
@@ -3,12 +3,12 @@ package edu.byu.cs.tweeter.server.service;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
 import edu.byu.cs.tweeter.model.net.request.LoginRequest;
-import edu.byu.cs.tweeter.model.net.response.LoginResponse;
+import edu.byu.cs.tweeter.model.net.response.AuthenticationResponse;
 import edu.byu.cs.tweeter.util.FakeData;
 
 public class UserService {
 
-    public LoginResponse login(LoginRequest request) {
+    public AuthenticationResponse login(LoginRequest request) {
         if(request.getUsername() == null){
             throw new RuntimeException("[Bad Request] Missing a username");
         } else if(request.getPassword() == null) {
@@ -18,7 +18,7 @@ public class UserService {
         // TODO: Generates dummy data. Replace with a real implementation.
         User user = getDummyUser();
         AuthToken authToken = getDummyAuthToken();
-        return new LoginResponse(user, authToken);
+        return new AuthenticationResponse(user, authToken);
     }
 
     /**
